d61ac0fac4fa76a62ad20217cace5e0804412bd0
Luke Amos <luke.amos.24@ucl.ac.uk>
2024-11-08 10:44:30 +0000

    Enabling autodoc + automatically including docstrings from squares.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ author = 'Luke Amos'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -25,3 +27,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
