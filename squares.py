236b22507be97ebce7e41386aa8d0d4387190e15
YMM <y7895809@163.com>
2024-11-10 23:49:18 +0000

    Correct the code and verify

--- squares.py
+++ squares.py
@@ -12,7 +12,7 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
     >>> average_of_squares([1, 2, 4])
     7.0
     >>> average_of_squares([2, 4], [1, 0.5])
-    6.0
+    8.0
     >>> average_of_squares([1, 2, 4], [1, 0.5])
     Traceback (most recent call last):
     AssertionError: weights and numbers must have same length
@@ -24,12 +24,14 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
         effective_weights = list_of_weights
     else:
         effective_weights = [1] * len(list_of_numbers)
+    
     squares = [
         weight * number * number
         for number, weight
         in zip(list_of_numbers, effective_weights)
     ]
-    return sum(squares)
+    total_weight = sum(effective_weights)
+    return sum(squares) / total_weight
 
 
 def convert_numbers(list_of_strings):
@@ -38,16 +40,16 @@ def convert_numbers(list_of_strings):
     Example:
     --------
     >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
-    [4, 8, 15, 16]
+    [4, 8, 15, 16, 23, 42]
 
     """
     all_numbers = []
     for s in list_of_strings:
         # Take each string in the list, split it into substrings separated by
         # whitespace, and collect them into a single list...
-        all_numbers.extend([token.strip() for token in s.split()])
+        all_numbers.extend(s.split())
     # ...then convert each substring into a number
-    return [float(number_string) for number_string in all_numbers]
+    return [int(number_string) for number_string in all_numbers]
 
 
 if __name__ == "__main__":
