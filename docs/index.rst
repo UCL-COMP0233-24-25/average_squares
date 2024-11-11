488e840ae2fa92d2866bc413738a1c703257d997
YMM <y7895809@163.com>
2024-11-11 00:27:20 +0000

    Add Sphinx documentation for Average Squares

---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. Average Squares documentation master file, created by
+   sphinx-quickstart on Sun Nov 10 23:57:30 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Average Squares's documentation!
+===========================================
+This project provides a utility for calculating the weighted average of squares
+of numbers, with additional functionality for data conversion.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   content/average-squares-docs
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- squares.py
+++ squares.py
@@ -2,21 +2,23 @@
 
 
 def average_of_squares(list_of_numbers, list_of_weights=None):
-    """ Return the weighted average of a list of values.
+    """Convert a list of strings into numbers, ignoring whitespace.
     
-    By default, all values are equally weighted, but this can be changed
-    by the list_of_weights argument.
-    
-    Example:
-    --------
-    >>> average_of_squares([1, 2, 4])
-    7.0
-    >>> average_of_squares([2, 4], [1, 0.5])
-    8.0
-    >>> average_of_squares([1, 2, 4], [1, 0.5])
-    Traceback (most recent call last):
-    AssertionError: weights and numbers must have same length
+    Parameters
+    ----------
+    list_of_strings : list of str
+        A list of strings where each string contains one or more numbers
+        separated by whitespace.
 
+    Returns
+    -------
+    list of int
+        A list of integers parsed from the input strings.
+
+    Examples
+    --------
+    >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
+    [4, 8, 15, 16, 23, 42]
     """
     if list_of_weights is not None:
         assert len(list_of_weights) == len(list_of_numbers), \
@@ -37,11 +39,21 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
 def convert_numbers(list_of_strings):
     """Convert a list of strings into numbers, ignoring whitespace.
     
-    Example:
+    Parameters
+    ----------
+    list_of_strings : list of str
+        A list of strings where each string contains one or more numbers
+        separated by whitespace.
+
+    Returns
+    -------
+    list of int
+        A list of integers parsed from the input strings.
+
+    Examples
     --------
     >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
     [4, 8, 15, 16, 23, 42]
-
     """
     all_numbers = []
     for s in list_of_strings:
